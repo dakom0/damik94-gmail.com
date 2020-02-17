062ab0cb9fe94b1727573b2c18a29f83d4f33fd4
john DIEKMIER <damik94@gmail.com>
2020-02-17 14:48:11 -0500

    making the game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,13 @@
+console.log("Up and running!");
+
+let cardOne = "queen";
+
+let cardTwo = "king";
+
+let cardThree = "queen";
+
+let cardFour = "king";
+
+console.log("User flipped " + cardOne);
+
+console.log("User flipped " + cardTwo);
