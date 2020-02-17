1641d78e5f828c636d923a9a011b2c50a2d9c432
john DIEKMIER <damik94@gmail.com>
2020-02-17 17:03:41 -0500

    making the game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,13 +1,29 @@
 console.log("Up and running!");
 
-let cardOne = "queen";
 
-let cardTwo = "king";
+cards = ["queen", "queen", "king", "king"];
 
-let cardThree = "queen";
+cardsInPlay = [];
 
-let cardFour = "king";
+cardOne = cards[0];
 
-console.log("User flipped " + cardOne);
+cardTwo = cards[2];
 
-console.log("User flipped " + cardTwo);
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+
+console.log("User flipped "+ cardOne);
+
+console.log("User flipped "+ cardTwo);
+
+if (cardsInPlay.length===2){
+  if(cardsInPlay[0]===cardsInPlay[1]){
+    alert("You found a match!");
+  }else{
+        alert("Sorry, try again.");
+
+  }
+  console.log("Two cards");
+}
